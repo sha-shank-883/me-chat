348a20e210453feaef8fa75da0a7747c3c5139de
shashank singhal <shashanksinghal883@gmail.com>
2023-03-23 00:54:41 +0530

    new chat-app

---
+++ Server/index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+// const path = require("path");
+const mongoose = require("mongoose");
+const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messagesRoute");
+const app = express();
+const socket = require("socket.io");
+require("dotenv").config();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("DB Connetion Successfull");
+  })
+  .catch((err) => {
+    console.log(err.message);
+  });
+
+const server = app.listen(PORT, () => {
+  console.log(`Server started on ${PORT}`);
+});
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("Client/build"));
+}
+
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(_dirname, " /Client/build")));
+//   app.get(" * ", (req, res) => {
+//     res.sendFile(path.join(_dirname, "Client", "build", "index.html"));
+//   });
+// } else {
+//   app.get(" / ", (req, res) => {
+//     res.send(" Api runnning ");
+//   });
+// }
+
+const io = socket(server, {
+  cors: {
+    origin: "*",
+    // origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+global.onlineUsers = new Map();
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recieve", data.message);
+    }
+  });
+});
